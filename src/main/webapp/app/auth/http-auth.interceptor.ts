6d8e636579a1685074230441decdea66bfd2a2be
Renan Meneses de Andrade Franca <renan.franca@tjse.jus.br>
2022-11-14 23:56:56 -0300

    add auth0 login workflow to angular app

--- src/main/webapp/app/auth/http-auth.interceptor.ts
+++ src/main/webapp/app/auth/http-auth.interceptor.ts
@@ -1,6 +1,7 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Oauth2AuthService } from './oauth2-auth.service';
 
 @Injectable()
@@ -8,14 +9,16 @@ export class HttpAuthInterceptor implements HttpInterceptor {
   constructor(private oauth2AuthService: Oauth2AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = this.oauth2AuthService.token;
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-    }
-    return next.handle(request);
+    return this.oauth2AuthService.getToken().pipe(
+      switchMap(token => { 
+        if (token) {
+          request = request.clone({
+            setHeaders: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
+        return next.handle(request);
+      }));
   }
 }
--- src/main/webapp/environments/environment.ts
+++ src/main/webapp/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
-  keycloak: {
-    url: 'http://localhost:9080',
-    realm: 'jhipster',
-    client_id: 'web_app'
+  auth0: {
+    domain: 'dev-yagoof6o4p0gf7rk.us.auth0.com',
+    clientId: 'Ycfc1uQ327fpi2BfecNotNLqiXcmCn1q',
+    audience: 'https://dev-yagoof6o4p0gf7rk.us.auth0.com/api/v2/',
+    redirectUri: window.location.origin
   },
 
   production: false
